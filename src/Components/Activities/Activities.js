c112828a1b79d7d31fe14a09f3c0ca1332a2110f
jaber1104076 <jabercuet11@gmail.com>
2022-09-29 20:11:12 +0600

    some responsivr feature added

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -89,7 +89,7 @@ const Activities = () => {
 
             </div>
             <div>
-                <h1 className='question-title'>Important Question and Answer About React</h1>
+                <h1 className='question-title'>Important Question and Answer <br />  About React</h1>
                 <div className='ans-cart'>
                     <h2>1. How does react work?</h2>
                     <p>React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.</p>
