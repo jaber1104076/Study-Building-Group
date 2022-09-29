002de0a69f84e500855507f56f7b8b1b8920e456
jaber1104076 <jabercuet11@gmail.com>
2022-09-29 08:01:45 +0600

    side Bar design

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -30,9 +30,39 @@ const Activities = () => {
                 <div className="cart-conatiner">
                     <div className='profile'>
                         <img src={jaber} alt="" />
-                        <h5>Abdullah Al Jaber</h5>
+                        <h5 className='title'>Abdullah Al Jaber</h5>
+                    </div>
+                    <div className="info">
+                        <div className='my-info'>
+                            <h3>70kg</h3>
+                            <p>Weight</p>
+                        </div>
+                        <div className='my-info'>
+                            <h3>5.5'</h3>
+                            <p>Height</p>
+                        </div>
+                        <div className='my-info'>
+                            <h3>28years</h3>
+                            <p>Age</p>
+                        </div>
+                    </div>
+                    <div className="break-info">
+                        <h2 className='title'>Add a break</h2>
+                        <div className="break-button">
+                            <button className='btn-break'>10s</button>
+                            <button className='btn-break'>20s</button>
+                            <button className='btn-break'>30s</button>
+                            <button className='btn-break'>40s</button>
+                        </div>
+                    </div>
+                    <div className="study-details">
+                        <h2 className='title'>Study Details</h2>
+                        <input type="text" placeholder='study time' className='input-field' />
+                        <input type="text" placeholder='Break-time' className='input-field' />
+                        <button className='activity-button'>Activity Completed</button>
                     </div>
                 </div>
+
             </div>
         </div>
     );
